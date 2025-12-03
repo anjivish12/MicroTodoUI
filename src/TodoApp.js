034577734371c83570cec363865a745c734c988c
anjali <anjali@gmail.com>
2025-12-03 17:14:30 +0530

    update url

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,9 +6,9 @@ import { Box } from '@mui/material';
 import config from './config'; // Import the config file with environment variable support
 
 // Use the config to define API URLs
-const GET_TASKS_API_BASE_URL = "http://add-todo-api.dhoomdhoom.shop";
+const GET_TASKS_API_BASE_URL = "http://get-todo-api.dhoomdhoom.shop";
 const DELETE_TASK_API_BASE_URL = "http://delete-todo-api.dhoomdhoom.shop";
-const CREATE_TASK_API_BASE_URL = "http://get-todo-api.dhoomdhoom.shop";
+const CREATE_TASK_API_BASE_URL = "http://add-todo-api.dhoomdhoom.shop";
 
 //Update Task Functionality is Work In Progress
 // const UPDATE_TASK_API_BASE_URL = '';
